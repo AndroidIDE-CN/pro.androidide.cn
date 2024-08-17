43eadb18c2b9a9bf7d6849837584a16d2b7e33fa
yichen9247 <865252486@qq.com>
2024-08-17 21:50:22 +0800

    hua aide-pro（close disenbold f12）

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,11 +1,11 @@
 
 import apiConfig from "./config"
-import developer from "./developer"
+// import developer from "./developer"
 import axiostool from "./axiostool"
 import siteConfig from "../../config/config"
-import DisableDevtool from 'disable-devtool'
+// import DisableDevtool from 'disable-devtool'
 import { useFunctionStore } from "@/stores/function"
 import { useApplicationStore } from "@/stores/application"
-if (import.meta.env.MODE === 'production') DisableDevtool(developer);
+// if (import.meta.env.MODE === 'production') DisableDevtool(developer);
 
 export default { apiConfig, axiostool, siteConfig, useFunctionStore, useApplicationStore }
