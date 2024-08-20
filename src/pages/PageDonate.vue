6f29b06ac5ae9717e6f74eb44e29d8b38406d42e
yichen9247 <865252486@qq.com>
2024-08-20 11:42:39 +0800

    hua aide-pro（add donate dialog）

--- src/pages/PageDonate.vue
+++ src/pages/PageDonate.vue
@@ -5,8 +5,10 @@
      */
 
     import utils from '@/scripts/utils'
+    import { useRoute } from 'vue-router'
     import { onMounted, ref, watch } from 'vue'
 
+    const route = useRoute();
     const payAction = ref('0');
     const currentPrice = ref('0');
     const keyboardStatus = ref(false);
@@ -38,7 +40,7 @@
 </script>
 
 <template>
-    <div :class="`page-donate ${applicationStore.isDeviceMobile && 'page-container'}`">
+    <div :class="`page-donate ${applicationStore.isDeviceMobile && 'page-container'}`" :style="{ paddingTop: route.query.from === 'app' ? '0px' : '10px' }">
         <!-- <header class="page-header">捐助我们</header> -->
 
         <van-grid :column-num="3" style="width: 95%; margin: 0 auto;">
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -92,19 +92,19 @@
         <HeadModel/><MainModel/><StatisticsFrame/><SelectntroFrame/><FriendsFrame/><FootModel/>
     </div>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isUseAgrOpen" title="使用协议" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isUseAgrOpen" title="使用协议" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)">
         <div class="innerHtml" v-html="modelConfig.useAgrHtml"></div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isPriAgrOpen" title="隐私政策" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isPriAgrOpen" title="隐私政策" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)">
         <div class="innerHtml" v-html="modelConfig.priAgrHtml"></div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isResAgrOpen" title="免责声明" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isResAgrOpen" title="免责声明" :width="applicationStore.isDeviceMobile ? '90%' : '720px'" @open="functionStore.setIsMobileHeadMore(false)">
         <div class="innerHtml" v-html="modelConfig.resAgrHtml"></div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isUpdeteOpen" title="更新日志" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isHisVerOpen" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isUpdeteOpen" title="更新日志" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isHisVerOpen">
         <div class="innerHtml" v-loading="!requestStatus.isHisVerOpen" :style="{ height: requestStatus.isHisVerOpen ? 'auto' : '200px' }">
             <div v-if="requestStatus.isHisVerOpen">
                 <h3 style="margin: 0;">版本信息</h3>
@@ -116,7 +116,7 @@
         </div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isHisVerOpen" title="历史版本" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isHisVerOpen" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isHisVerOpen" title="历史版本" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isHisVerOpen">
         <div class="innerHtml" v-loading="!requestStatus.isHisVerOpen" :style="{ height: requestStatus.isHisVerOpen ? 'auto' : '200px' }">
             <el-collapse v-model="historyModel">
                 <el-collapse-item v-for="(item,index) in requestDatas.isHisVerOpen" :key="index" :title="`${item.version}（${item.vercode}）`" :name="item.vercode">
@@ -126,7 +126,7 @@
         </div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isThanksOpen" title="特别鸣谢" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isThanksOpen" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isThanksOpen" title="特别鸣谢" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isThanksOpen">
         <div class="special-thanks" v-loading="!requestStatus.isThanksOpen" :style="{ height: requestStatus.isThanksOpen ? 'auto' : '200px' }">
             <div class="special-thanks-item" v-for="(item,index) in requestDatas.isThanksOpen" :key="index">
                 <img class="user-avatar" v-lazy="`https://q2.qlogo.cn/g?b=qq&s=640&nk=${item.qq}`" :alt="item.nick">
@@ -138,13 +138,13 @@
         </div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isContacOpen" title="联系我们" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isContacOpen" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isContacOpen" title="联系我们" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isContacOpen">
         <div class="contack-us" v-loading="!requestStatus.isContacOpen" :style="{ height: requestStatus.isContacOpen ? 'auto' : '200px' }">
             <van-cell  v-for="(item,index) in requestDatas.isContacOpen" :key="index" :title="item.name" :label="item.desc" class="contack-item" @click="letGoContack(item.href)"/>
         </div>
     </el-dialog>
 
-    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isSupporOpen" title="支持人员" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isSupporOpen" :close-on-press-escape="false" :close-on-click-modal="false">
+    <el-dialog align-center destroy-on-close class="application-dialog" v-model="functionStore.isSupporOpen" title="支持人员" :width="applicationStore.isDeviceMobile ? '90%' : '520px'" @open="isSupporOpen">
         <div class="special-thanks" v-loading="!requestStatus.isSupporOpen" :style="{ height: requestStatus.isSupporOpen ? 'auto' : '200px' }">
             <div class="special-thanks-item" v-for="(item,index) in requestDatas.isSupporOpen" :key="index">
                 <img class="user-avatar" v-lazy="item.img" :alt="item.name">
